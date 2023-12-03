601c265f6c718ce1a36c6fdeb116093687228792
konkinit <ikonkobo.idrissa@gmail.com>
2023-12-03 20:18:58 +0000

    data reading from s3 bucket

---
+++ src/core/configs.py
@@ -0,0 +1,16 @@
+import os
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class GlobalSettings(BaseModel):
+    s3_endpointUrl: str = os.getenv('S3_ENDPOINT_URL')
+    s3_key: str = os.getenv('S3_KEY')
+    s3_secret: str = os.getenv('S3_SECRET')
+    s3_token: str = os.getenv('S3_TOKEN')
+
+
+settings = GlobalSettings()
