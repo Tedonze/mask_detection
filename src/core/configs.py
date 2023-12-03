bb5e4487f2d68ee4142871e0ae1f9f3506fe844f
konkinit <ikonkobo.idrissa@gmail.com>
2023-12-03 20:33:24 +0000

    setting pytorch dataloaders

--- src/core/configs.py
+++ src/core/configs.py
@@ -11,6 +11,7 @@ class GlobalSettings(BaseModel):
     s3_key: str = os.getenv('S3_KEY')
     s3_secret: str = os.getenv('S3_SECRET')
     s3_token: str = os.getenv('S3_TOKEN')
+    s3_prefix: str = 'ikonkobo/covid19_pis_data/'
 
 
 settings = GlobalSettings()
